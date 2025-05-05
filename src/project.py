d6f78d5d07d828382176700b1458528fc69e3bb5
Ashley Simonson <aesimonson0219@gmail.com>
2025-05-05 17:37:54 -0500

    small tweak to player hitbox

--- src/project.py
+++ src/project.py
@@ -21,6 +21,7 @@ class Player():
         self.rect = self.image.get_rect(center = pos)
         self.rect.center = pos
         self.hitbox = self.rect.copy().inflate((-32, -80))
+        self.hitbox.center = pos
 
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2((pos[0]-32, pos[1]-64))
